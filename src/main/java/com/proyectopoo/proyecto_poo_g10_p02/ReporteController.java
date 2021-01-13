7b132d4b50324cfd36280bb189a032ae7e89e6a9
andre <andre@192.168.39.154>
2021-01-12 19:13:08 -0500

    Agregando funcion a parte grafica

--- src/main/java/com/proyectopoo/proyecto_poo_g10_p02/ReporteController.java
+++ src/main/java/com/proyectopoo/proyecto_poo_g10_p02/ReporteController.java
@@ -13,14 +13,10 @@ package com.proyectopoo.proyecto_poo_g10_p02;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class MenuPrincipalController {
-    @FXML
-    private void cambiarConfiguracion() throws IOException{
-        App.setRoot("Configuraciones");
-    }
+public class ReporteController {
     
     @FXML
-    private void cambiarReporte() throws IOException{
-        App.setRoot("Reporte");
+    private void regresarM() throws IOException{
+        App.setRoot("Menu_Principal");
     }
 }
